0f1959da960bb2c29564228c0c78266fae413f2c
EmanDomo <domooseman31@gmail.com>
2025-03-27 09:21:26 +0800

    change

--- client/src/apiRoutes.js
+++ client/src/apiRoutes.js
@@ -1,5 +1,5 @@
 const useProduction = true; // Change to false to use localhost
 
 export const host = useProduction
-  ? "https://sdo-deped-cabuyao-ticketing-system-2.onrender.com/"
+  ? "https://sdo-deped-cabuyao-ticketing-system-1.onrender.com"
   : "http://localhost:8080"; 
